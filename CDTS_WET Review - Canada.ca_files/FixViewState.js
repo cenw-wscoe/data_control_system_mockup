7b2283fe606703fa469419ac562f2500034c9a13
Maxim Perry <maximperry@Maxims-Tower.local>
2020-04-21 08:25:17 -0400

    Initial commit

---
+++ CDTS_WET Review - Canada.ca_files/FixViewState.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function()
+{
+    if (typeof __doPostBack == "function")
+    {
+        var __doPostBack_original = __doPostBack;
+
+        __doPostBack = function(eventTarget, eventArgument)
+        {
+            $("form").each(function()
+            {
+                var viewStates = $(this).find("[name='__VIEWSTATE']");
+
+                if (viewStates.length > 1)
+                {
+                    viewStates.slice(0, viewStates.length - 1).remove();
+                }
+            });
+
+            __doPostBack_original(eventTarget, eventArgument);
+        }
+    }
+});
